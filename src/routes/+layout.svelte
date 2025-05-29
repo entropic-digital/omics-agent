78fe2014b11c76409fdc79a3e307e59834485ebe
dionizijefa <dionizije.fa@hotmail.com>
2025-05-30 00:32:27 +0200

    Some branding changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -432,7 +432,7 @@
 			if (type === 'message') {
 				if ($isLastActiveTab) {
 					if ($settings?.notificationEnabled ?? false) {
-						new Notification(`${data?.user?.name} (#${event?.channel?.name}) • Open WebUI`, {
+						new Notification(`${data?.user?.name} (#${event?.channel?.name})`, {
 							body: data?.content,
 							icon: data?.user?.profile_image_url ?? `${WEBUI_BASE_URL}/static/favicon.png`
 						});
